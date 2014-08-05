aa59a012d47f127e2ab8a43a687d01dc2b4c6c39
Yaroslav Ulanovych <Yaroslav_Ulanovych@epam.com>
2014-08-05 17:19:06 +0300

    empty project

---
+++ src/main/scala/com/example/Example.scala
@@ -0,0 +1,7 @@
+package com.example
+
+object Example {
+	def main(args: Array[String]) {
+		println("Hello World!")
+	}
+}
---
+++ src/test/scala/com.example/ExampleTest.scala
@@ -0,0 +1,9 @@
+package com.example
+
+import org.scalatest.FunSuite
+
+class ExampleTest extends FunSuite {
+  test("test") {
+    assert(true)
+  }
+}
